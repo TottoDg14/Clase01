c3f2d0da3a1507158342daa0c8cff0ea8d26f6c4
jjvector <juan_abreu_2005@hotmail.com>
2025-10-17 17:18:08 -0400

    Add nuevas rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 // zona de importacion
 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
 import Movil from "./pages/Movil"
 import Laptop from "./pages/Laptop"
-import Tienda from "./pages/Tienda"
+import Store from "./pages/Store"
 import Detalle from "./pages/Detalle"
 import Category from "./pages/Category"
+import Search from "./pages/Search"
+import Table from "./pages/Table"
 
 const App = () => {
   // zona de logica
-  
   
   // zona de renderizado
   return (
@@ -25,9 +26,12 @@ const App = () => {
             <Route path="/inicio" element={<Home/>}/>
             <Route path="/movil" element={<Movil/>}/>
             <Route path="/laptop" element={<Laptop/>}/>
-            <Route path="/tienda" element={<Tienda/>}/>
+            <Route path="/tienda" element={<Store/>}/>
+            <Route path="/tabla" element={<Table/>}/>
             <Route path="/categorias/:category" element={<Category/>}/>
+
             <Route path="/detalle/:id/:title" element={<Detalle/>}/>
+            <Route path="/busquedas" element={<Search/>}/>
             <Route path="*" element={'404 - No se encontro el contenido.'}/>
           </Routes>
         </div>
